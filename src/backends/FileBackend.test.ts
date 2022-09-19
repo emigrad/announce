40ffa767a8f6315006834b82b5a0c4f66fdf6fa4
Stefan Bird <stefan.bird@emigrad.com>
2022-09-20 07:27:33 +1000

    feat(FileBackend): support multiple processes

--- src/backends/FileBackend.test.ts
+++ src/backends/FileBackend.test.ts
@@ -1,3 +1,4 @@
+import assert from 'assert'
 import { createHash } from 'crypto'
 import { tmpdir } from 'os'
 import { resolve } from 'path'
@@ -5,7 +6,13 @@ import rimrafCb from 'rimraf'
 import { Deferred } from 'ts-deferred'
 import { promisify } from 'util'
 import { BackendSubscriber, Message } from '../types'
-import { createMessage, getCompleteMessage, getConcurrency } from '../util'
+import {
+  createMessage,
+  getCompleteMessage,
+  getConcurrency,
+  getDeadLetterQueueName,
+  getDeadLetterTopic
+} from '../util'
 import { FileBackend } from './FileBackend'
 
 const rimraf = promisify(rimrafCb)
@@ -60,18 +67,25 @@ describe('File backend', () => {
   ])(
     'Should support wildcards in topic selectors (selector: %p, topic: %p)',
     async (selector, topic, expected) => {
+      const delay = 200
+      const deferred = new Deferred<boolean>()
       let receivedMessage = false
       const subscriber: BackendSubscriber = {
         queueName: 'test',
         topics: [selector],
         handle: () => {
           receivedMessage = true
+          deferred.resolve(true)
+          clearTimeout(timeout)
         }
       }
       const message = createMessage(topic, Buffer.from('hi there'))
+      const timeout = setTimeout(() => deferred.resolve(false), delay)
 
       await fileBackend.subscribe(subscriber)
       await fileBackend.publish(getCompleteMessage(message))
+
+      await deferred.promise
 
       expect(receivedMessage).toBe(expected)
     }
@@ -94,7 +108,6 @@ describe('File backend', () => {
       queueName: 'test',
       topics: ['foo'],
       handle: async ({ body }) => {
-        expect(numRunning).toBeLessThan(getConcurrency(subscriber))
         numRunning++
         maxRunning = Math.max(maxRunning, numRunning)
 
@@ -114,35 +127,7 @@ describe('File backend', () => {
     )
     await done
 
-    expect(maxRunning).toBe(subscriber.options?.concurrency)
-  })
-
-  it('Should redeliver messages that did not complete processing', async () => {
-    const dfd = new Deferred()
-    const subscriber1: BackendSubscriber = {
-      queueName: 'test',
-      topics: ['foo.bar'],
-      handle: () =>
-        new Promise(() => {
-          // Never resolve
-        })
-    }
-    const subscriber2: BackendSubscriber = {
-      queueName: 'test',
-      topics: ['foo.bar'],
-      handle: () => dfd.resolve()
-    }
-    const message = createMessage('foo.bar', Buffer.from('hi there'))
-
-    const fileBackend1 = new FileBackend(path)
-    await fileBackend1.subscribe(subscriber1)
-    await fileBackend1.publish(getCompleteMessage(message))
-
-    const fileBackend2 = new FileBackend(path)
-    await fileBackend2.subscribe(subscriber2)
-
-    await dfd.promise
-    await Promise.all([fileBackend1.close(), fileBackend2.close()])
+    expect(maxRunning).toBe(getConcurrency(subscriber))
   })
 
   it('Should not redeliver messages that were successfully processed', async () => {
@@ -186,9 +171,12 @@ describe('File backend', () => {
           return Promise.reject()
         }
       }
+      const dlqName = getDeadLetterQueueName(subscriber)
+      const dlqTopic = getDeadLetterTopic(subscriber)
+      assert(dlqName && dlqTopic)
       const dlqSubscriber: BackendSubscriber = {
-        queueName: 'test.dlq',
-        topics: [],
+        queueName: dlqName,
+        topics: [dlqTopic],
         handle: () => dlqSubscriberDfd.resolve()
       }
       const message = getCompleteMessage({
@@ -212,4 +200,13 @@ describe('File backend', () => {
       await subscriberDfd.promise
     }
   )
+
+  it.todo('should support subscriber and publisher being on separate processes')
+  it.todo('should recover crashed messages')
+  it.todo('should not recover messages that are still being processed')
+  it.todo('should handle redefining queues')
+  it.todo('should handle queues being redefined by another process')
+  it.todo('should handle queues being removed')
+  it.todo('message processing stress test')
+  it.todo('should handle messages that were added before startup')
 })
--- src/backends/FileBackend.ts
+++ src/backends/FileBackend.ts
@@ -1,41 +1,60 @@
 import assert from 'assert'
-import cuid from 'cuid'
+import chokidar, { FSWatcher } from 'chokidar'
+import createDebug from 'debug'
+import { EventEmitter } from 'events'
 import {
+  close as closeCb,
   mkdir as mkdirCb,
+  open as openCb,
   readdir as readdirCb,
   readFile as readFileCb,
+  rename as renameCb,
+  stat as statCb,
   unlink as unlinkCb,
   writeFile as writeFileCb
 } from 'fs'
-import { resolve } from 'path'
+import { basename, dirname, join, resolve } from 'path'
+import PromiseQueue from 'promise-queue'
+import { prop } from 'rambda'
 import { promisify } from 'util'
-import { getDeadLetterTopic, hasDeadLetterTopic } from '../util'
-import { BackendSubscriber, Message, Subscriber } from '../types'
-import { LocalBackend, SubscriberWithQueue } from './LocalBackend'
+import { BackendSubscriber, Message } from '../types'
+import { getConcurrency, getDeadLetterTopic, hasDeadLetterTopic } from '../util'
 
+const debug = createDebug('announce:FileBackend')
+
+const close = promisify(closeCb)
 const mkdir = promisify(mkdirCb)
+const open = promisify(openCb)
 const readdir = promisify(readdirCb)
 const readFile = promisify(readFileCb)
+const rename = promisify(renameCb)
+const stat = promisify(statCb)
 const unlink = promisify(unlinkCb)
 const writeFile = promisify(writeFileCb)
 
-const fileIdHeader = 'x-file-id'
+const KEEPALIVE_INTERVAL = 30000
 
 /**
  * A backend that's backed by the filesystem to ensure at-least-once delivery.
- * This backend is suitable for light production use where there is only a
- * single instance of the backend running.
+ * It is designed for use with local file systems only - using a network drive
+ * is not supported because the operating systemn doesn't always provide
+ * notification that files have changed. In environments where there are
+ * multiple instances of the application running across a network, it is
+ * strongly recommended you use a dedicated message broker such as RabbitMQ
+ * instead
  *
  * Supports:
  *  - guaranteed delivery
  *  - dead letter queues
- *
- * Does not support:
- *  - inter-service messaging
- *  - multiple instances
  */
-export class FileBackend extends LocalBackend {
+export class FileBackend extends EventEmitter {
   private readonly queuePath: string
+  private readonly subscriptionsPath: string
+  private subscriptionsWatcher!: FSWatcher
+  private readonly externalSubscribers: Record<string, ExternalSubscriber> = {}
+  private readonly startupPromise: Promise<void>
+  private readonly processingTimesByPath: Record<string, number> = {}
+  private readonly queuesByName: Record<string, Queue> = {}
 
   static accepts(url: string) {
     return url.startsWith('file://')
@@ -45,115 +64,95 @@ export class FileBackend extends LocalBackend {
     super()
 
     this.queuePath = resolve(path, 'queues')
+    this.subscriptionsPath = resolve(path, 'subscriptions')
+
+    this.startupPromise = this.startup()
+    this.startupPromise.then(
+      () => {
+        debug(`Backend ready: ${path}`)
+      },
+      (e) => {
+        debug(`Startup failed: ${e}`)
+        this.emit('error', e)
+      }
+    )
   }
 
   async subscribe(subscriber: BackendSubscriber): Promise<void> {
-    await super.subscribe(subscriber)
-    await this.queuePersistedMessages(subscriber)
+    await this.startupPromise
+    await this.writeSubscriber(subscriber)
+
+    // Make sure the queue has been fully created before we proceed so that
+    // there is a directory to watch
+    await this.onSubscriberChanged(this.getSubscriberPath(subscriber))
+
+    const queueName = subscriber.queueName
+    const queuePath = this.getQueuePath(queueName)
+    const existingQueue = this.queuesByName[queueName]
+    if (existingQueue) {
+      await existingQueue.watcher.close()
+    }
+
+    const watcher = chokidar
+      .watch(queuePath, { atomic: false })
+      .on('add', (path) => this.onMessageAdded(path))
+      .on('change', (path) => this.onMessageTouched(path))
+      .on('unlink', (path) => this.onMessageUnlinked(path))
+
+    this.queuesByName[queueName] = {
+      name: queueName,
+      subscriber,
+      watcher,
+      pendingMessages: [],
+      processingQueue: new PromiseQueue(getConcurrency(subscriber))
+    }
+
+    await this.loadExistingMessages(queueName)
+
+    debug(`Watching for messages in ${queuePath}`)
   }
 
   async publish(message: Message<Buffer>): Promise<void> {
+    await this.startupPromise
+    const queueNames = Object.values(this.externalSubscribers)
+      .filter(subscribesTo(message.topic))
+      .map(prop('queueName'))
+
     await Promise.all(
-      this.getMatchingSubscribers(message).map(async (subscriber) => {
-        await this.enqueue(message, subscriber.queueName)
-      })
+      queueNames.map((queueName) =>
+        this.addToPersistedQueue(message, queueName)
+      )
     )
+
+    debug(`Published message ${message.properties.id}`)
   }
 
-  /**
-   * Loads all of the persisted messages into the in-memory queue
-   */
-  private async queuePersistedMessages(
-    subscriber: Subscriber<Buffer>
-  ): Promise<void> {
-    const persistedMessages = await this.loadPersistedMessages(subscriber)
+  async close(): Promise<void> {
+    await this.startupPromise
+    const watchers = [
+      this.subscriptionsWatcher,
+      ...Object.values(this.queuesByName).map(prop('watcher'))
+    ]
 
-    persistedMessages.forEach((message) =>
-      this.addToInMemoryQueue(message, subscriber.queueName)
-    )
+    await Promise.all(watchers.map((watcher) => watcher.close()))
+
+    debug('Closed')
   }
 
-  /**
-   * Loads all of the messages that have been persisted for the given
-   * subscriber
-   */
-  private async loadPersistedMessages(
-    subscriber: Subscriber<Buffer>
-  ): Promise<Message<Buffer>[]> {
-    const messageIds = await this.getPersistedMessageIds(subscriber)
+  private async startup(): Promise<void> {
+    await mkdir(this.subscriptionsPath, { recursive: true })
 
-    return Promise.all(
-      messageIds.map((id) => this.loadPersistedMessage(subscriber, id))
-    )
-  }
+    this.subscriptionsWatcher = chokidar
+      .watch(this.subscriptionsPath)
+      .on('add', (path) => this.onSubscriberChanged(path))
+      .on('change', (path) => this.onSubscriberChanged(path))
+      .on('unlink', (path) => this.onSubscriberRemoved(path))
 
-  /**
-   * Retrieves the IDs of the messages that have been persisted for the
-   * given queue. The message IDs will be returned oldest first
-   */
-  private async getPersistedMessageIds(
-    subscriber: Subscriber<Buffer>
-  ): Promise<string[]> {
-    try {
-      return (await readdir(this.getQueuePath(subscriber.queueName))).sort()
-    } catch (e: unknown) {
-      if ((e as { code: string }).code === 'ENOENT') {
-        return []
-      } else {
-        throw e
-      }
-    }
-  }
+    await this.loadExternalSubscribers()
 
-  /**
-   * Loads a persisted message from disk
-   */
-  private async loadPersistedMessage(
-    subscriber: Subscriber<Buffer>,
-    id: string
-  ): Promise<Message<Buffer>> {
-    const path = this.getMessagePathFromId(id, subscriber.queueName)
-    const rawMessage = await readFile(path, 'utf8')
-
-    const parsedMessage = JSON.parse(rawMessage) as Message<string>
-    parsedMessage.properties.date = new Date(parsedMessage.properties.date)
-    return { ...parsedMessage, body: Buffer.from(parsedMessage.body, 'base64') }
-  }
-
-  /**
-   * Adds the message to both the persisted and in-memory queues
-   */
-  private async enqueue(
-    message: Message<Buffer>,
-    queue: string
-  ): Promise<void> {
-    message = addMetadata(message)
-
-    await this.addToPersistedQueue(message, queue)
-    this.addToInMemoryQueue(message, queue)
-  }
-
-  /**
-   * Adds the message to the in-memory queue
-   */
-  private addToInMemoryQueue(message: Message<Buffer>, queue: string) {
-    const subscriber = this.getSubscriberForQueue(queue)
-    if (!subscriber) {
-      return
-    }
-
-    subscriber.queue
-      .add(() => this.handleMessage(message, subscriber))
-      .catch((e) => this.emit('error', e))
-  }
-
-  /**
-   * Gets the subscriber that handles messages for the given queue, if any
-   */
-  private getSubscriberForQueue(
-    queue: string
-  ): SubscriberWithQueue | undefined {
-    return this.subscribers.find(({ queueName }) => queueName === queue)
+    // TODO: restore crashed messages. We need to check that the file actually
+    // has some contents, so that we don't accidentally restore a deleted
+    // file that was recreated by touch()
   }
 
   /**
@@ -163,43 +162,187 @@ export class FileBackend extends LocalBackend {
     message: Message<Buffer>,
     queue: string
   ): Promise<void> {
-    await this.createQueuePath(queue)
+    const path = this.getMessagePath(message, queue)
+    const contents = JSON.stringify({
+      ...message,
+      body: message.body.toString('base64')
+    })
 
-    return writeFile(
-      this.getMessagePath(message, queue),
-      JSON.stringify({ ...message, body: message.body.toString('base64') })
+    await writeFile(path, contents)
+    debug(`Wrote message ${message.properties.id} to ${path}`)
+  }
+
+  private async writeSubscriber(subscriber: BackendSubscriber): Promise<void> {
+    await writeFile(
+      this.getSubscriberPath(subscriber),
+      serializeSubscriber(subscriber)
+    )
+
+    debug(`Registered subscriber ${JSON.stringify(subscriber)}`)
+  }
+
+  private async onSubscriberChanged(path: string): Promise<void> {
+    const fileContents = await readFile(path)
+    let externalSubscriber: ExternalSubscriber
+
+    try {
+      externalSubscriber = deserializeSubscriber(fileContents.toString())
+    } catch {
+      // We can sometimes get incomplete details because we read the file
+      // before it had finished writing. If that's the case, ignore the
+      // subscriber and wait for the next update event
+      return
+    }
+
+    await this.createQueuePath(externalSubscriber.queueName)
+    this.externalSubscribers[externalSubscriber.queueName] = externalSubscriber
+
+    debug(`Subscriber added or changed: ${path}`)
+  }
+
+  private async onSubscriberRemoved(path: string): Promise<void> {
+    delete this.externalSubscribers[this.getQueueNameFromSubscriberPath(path)]
+
+    debug(`Subscriber removed: ${path}`)
+  }
+
+  private async loadExternalSubscribers(): Promise<void> {
+    const subscriptionFiles = await readdir(
+      this.getQueuePath(this.subscriptionsPath)
+    )
+
+    await Promise.all(
+      subscriptionFiles.map((path) =>
+        this.onSubscriberChanged(join(this.subscriptionsPath, path))
+      )
     )
   }
 
-  /**
-   * Gets the subscriber to handle the message
-   */
-  private async handleMessage(
-    message: Message<Buffer>,
-    subscriber: SubscriberWithQueue
-  ) {
-    try {
-      await subscriber.handle(message)
-    } catch (e) {
-      if (hasDeadLetterTopic(subscriber)) {
-        const deadLetterTopic = getDeadLetterTopic(subscriber)
-        assert(deadLetterTopic)
+  private async onMessageAdded(path: string): Promise<void> {
+    const filename = basename(path)
+    const queueName = basename(dirname(path))
 
-        await this.enqueue(message, deadLetterTopic)
-      }
+    if (filename.includes('.%processing.')) {
+      await this.onMessageTouched(path)
+    } else {
+      debug(`Message added: ${path}`)
+      this.queuesByName[queueName].pendingMessages.push(path)
+      await this.processNextMessage(queueName)
+    }
+  }
+
+  private async processNextMessage(queueName: string): Promise<void> {
+    const queue = this.queuesByName[queueName] as Queue | undefined
+
+    if (
+      !queue ||
+      !queue.pendingMessages.length ||
+      queue.processingQueue.getQueueLength() >= getConcurrency(queue.subscriber)
+    ) {
+      return
     }
 
-    await this.removeFromPersistedQueue(message, subscriber.queueName)
+    const messagePath = queue.pendingMessages.shift()
+    assert(messagePath)
+
+    const processingPath = messagePath.replace(/\.json$/, '.%processing.json')
+
+    queue.processingQueue
+      .add(async () => {
+        try {
+          // Attempt to "lock" the file
+          await rename(messagePath, processingPath)
+
+          await this.processMessage(queue, processingPath)
+        } catch (e: unknown) {
+          if ((e as { code?: string })?.code !== 'ENOENT') {
+            throw e
+          }
+        }
+      })
+      .finally(() => this.processNextMessage(queueName))
+  }
+
+  private async processMessage(
+    queue: Queue,
+    messagePath: string
+  ): Promise<void> {
+    const watchdog = setInterval(async () => {
+      // TODO: unhandled promise
+      await touch(messagePath)
+    }, KEEPALIVE_INTERVAL / 5)
+
+    try {
+      const message = await this.loadMessage(messagePath)
+      const subscriber = queue.subscriber
+
+      try {
+        await subscriber.handle(message)
+      } catch (e) {
+        if (hasDeadLetterTopic(subscriber)) {
+          const deadLetterTopic = getDeadLetterTopic(subscriber)
+          assert(deadLetterTopic)
+
+          // TODO: Publish to DLQ/DLT
+        }
+      }
+    } finally {
+      await unlink(messagePath)
+      clearInterval(watchdog)
+    }
   }
 
   /**
-   * Removes the message from the given queue on dist
+   * Loads a persisted message from disk
    */
-  private removeFromPersistedQueue(
-    message: Message<Buffer>,
-    queue: string
-  ): Promise<void> {
-    return unlink(this.getMessagePath(message, queue))
+  private async loadMessage(path: string): Promise<Message<Buffer>> {
+    const rawMessage = await readFile(path, 'utf8')
+
+    const parsedMessage = JSON.parse(rawMessage) as Message<string>
+    parsedMessage.properties.date = new Date(parsedMessage.properties.date)
+    return { ...parsedMessage, body: Buffer.from(parsedMessage.body, 'base64') }
+  }
+
+  private async onMessageTouched(path: string): Promise<void> {
+    const filename = basename(path)
+
+    if (filename.includes('.%processing.')) {
+      try {
+        const stats = await stat(path)
+        debug(`Processing flag updated: ${path}`)
+        this.processingTimesByPath[path] = stats.ctimeMs
+      } catch {
+        // The file has already been removed so we no longer need to track it
+      }
+    }
+  }
+
+  private async onMessageUnlinked(path: string): Promise<void> {
+    const filename = basename(path)
+    const queueName = basename(dirname(path))
+    const queue = this.queuesByName[queueName]
+
+    if (!queue) {
+      return
+    } else if (filename.includes('.%processing.')) {
+      delete this.processingTimesByPath[path]
+      debug(`Processing flag removed: ${path}`)
+    } else {
+      const unprocessedPaths = queue.pendingMessages
+
+      for (let idx = unprocessedPaths.length - 1; idx >= 0; idx--) {
+        if (unprocessedPaths[idx] === path) {
+          unprocessedPaths.splice(idx, 1)
+        }
+      }
+      debug(`Message removed: ${path}`)
+    }
+  }
+
+  private async loadExistingMessages(queueName: string): Promise<void> {
+    const paths = await readdir(this.getQueuePath(queueName))
+
+    paths.sort().forEach((path) => this.onMessageAdded(path))
   }
 
   /**
@@ -220,24 +363,60 @@ export class FileBackend extends LocalBackend {
   /**
    * Returns the path of the message in the given queue
    */
-  private getMessagePath(message: Message<Buffer>, queue: string): string {
-    return this.getMessagePathFromId(message.headers[fileIdHeader], queue)
+  private getMessagePath(message: Message<Buffer>, queueName: string): string {
+    return join(
+      this.getQueuePath(queueName),
+      `${encodeURIComponent(message.properties.id)}.json`
+    )
   }
 
-  /**
-   * Returns the path corresponding to the given message ID and queue
-   */
-  private getMessagePathFromId(messageId: string, queue: string): string {
-    return resolve(this.getQueuePath(queue), messageId)
+  private getQueueNameFromSubscriberPath(path: string): string {
+    return decodeURIComponent(basename(path).replace(/\.json$/, ''))
+  }
+
+  private getSubscriberPath(subscriber: ExternalSubscriber) {
+    return join(
+      this.subscriptionsPath,
+      `${encodeURIComponent(subscriber.queueName)}.json`
+    )
   }
 }
 
-/**
- * Adds our metadata to the message
- */
-function addMetadata(message: Message<Buffer>): Message<Buffer> {
-  return {
-    ...message,
-    headers: { ...message.headers, [fileIdHeader]: cuid() }
-  }
+function serializeSubscriber({ queueName, topics }: BackendSubscriber): string {
+  return JSON.stringify({ queueName, topics })
+}
+
+function deserializeSubscriber(str: string): ExternalSubscriber {
+  return JSON.parse(str)
+}
+
+function subscribesTo(
+  topic: string
+): (subscriber: ExternalSubscriber) => boolean {
+  return (subscriber) =>
+    subscriber.topics.some((topicSelector) =>
+      getTopicSelectorRegExp(topicSelector).test(topic)
+    )
+}
+
+function getTopicSelectorRegExp(topicSelector: string): RegExp {
+  const regExpStr = topicSelector
+    .replace(/\./g, '\\.')
+    .replace(/\*\*?/g, (match) => (match === '**' ? '.*' : '[^.]+'))
+
+  return new RegExp(`^${regExpStr}$`)
+}
+
+async function touch(path: string): Promise<void> {
+  await close(await open(path, 'w'))
+}
+
+type ExternalSubscriber = Pick<BackendSubscriber, 'queueName' | 'topics'>
+
+interface Queue {
+  name: string
+  subscriber: BackendSubscriber
+  watcher: FSWatcher
+  pendingMessages: string[]
+  processingQueue: PromiseQueue
 }
